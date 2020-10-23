0fdd09be421fabdd0abbcf0253536da27ff042b1
rhu <robert.hu@stats.ox.ac.uk>
2020-10-23 18:00:35 +0200

    updated parameters

--- jobs_normal.py
+++ jobs_normal.py
@@ -9,7 +9,17 @@ def parse_job(l_p,n,min_points,ref_size,a,b,ls,string):
 if __name__ == '__main__':
     for l_p in [4,5,6,7]:
         for n in [100000,1000000,10000000,100000000]:
-            for min_points in [250,2500,5000]:
+            if n==100000:
+                min_points_list = [100,500]
+            elif n==1000000:
+                min_points_list = [250,1000]
+            elif n==10000000:
+                min_points_list = [1000,10000]
+            elif n==100000000:
+                min_points_list = [10000,25000]
+            elif n==1000000000:
+                min_points_list = [10000,25000,100000]
+            for min_points in min_points_list:
                 for a,b in zip([0,100,-100],[1,10,100]):
                     for ls in [0.1,0.5,1,10,200,4000]:
                         bash = parse_job(l_p=l_p,n=n,min_points=min_points,ref_size=5000,a=a,b=b,ls=ls,string='normal_d=3')
