1f0c4d261f4d4102a12c811dd91155886e320449
rhu <robert.hu@stats.ox.ac.uk>
2021-04-17 22:30:54 +0200

    ok

---
+++ FFM_classes.py
@@ -0,0 +1,148 @@
+import torch
+from torch.utils.cpp_extension import load
+load_obj = load(name='ffm_3d_float', sources=['pybinder_setup.cu'])
+
+
+class FFM:
+    def __init__(self,
+                 X,
+                 Y=None,
+                 ls=1.0,
+                 min_points=1000,
+                 nr_of_interpolation=64,
+                 eff_var_limit=0.15,
+                 var_compression=False,
+                 smooth_interpolation=False,
+                 device = "cuda:0"
+                 ):
+        self.X = X.to(device).float()
+        if torch.is_tensor(Y):
+            self.Y = Y.to(device).float()
+        else:
+            self.Y = Y
+        self.d = self.X.shape[1]
+        try:
+            assert self.d>0 and self.d<6
+        except AssertionError:
+            print('Sorry bro, dimensionality of your data is too big; Can only do up to 5')
+        self.ls = float(ls)
+        self.min_points = float(min_points)
+        self.nr_of_interpolation = int(nr_of_interpolation)
+        self.eff_var_limit=float(eff_var_limit)
+        self.var_compression = var_compression
+        self.smooth_interpolation = smooth_interpolation
+        self.device = device
+
+    def update_ls(self,ls):
+        try:
+            assert ls>0
+        except AssertionError:
+            print('ls less than 0, not allowed')
+        self.ls = float(ls)
+
+    def __matmul__(self, b):
+        b = b.to(self.device).float()
+        if torch.is_tensor(self.Y):
+            try:
+                assert self.Y.shape[1]==self.X.shape[1]
+                assert self.Y.shape[0]==b.shape[0]
+            except AssertionError:
+                print('hey check the shapes of your tensor X,Y and b they dont match up!')
+            if self.d==1:
+                return load_obj.FFM_XY_FLOAT_1(self.X,self.Y,b,self.device,self.ls,self.min_points,self.nr_of_interpolation)
+            if self.d==2:
+                return load_obj.FFM_XY_FLOAT_2(self.X,self.Y,b,self.device,self.ls,self.min_points,self.nr_of_interpolation)
+            if self.d==3:
+                return load_obj.FFM_XY_FLOAT_3(self.X,self.Y,b,self.device,self.ls,self.min_points,self.nr_of_interpolation)
+            if self.d==4:
+                return load_obj.FFM_XY_FLOAT_4(self.X,self.Y,b,self.device,self.ls,self.min_points,self.nr_of_interpolation)
+            if self.d==5:
+                return load_obj.FFM_XY_FLOAT_5(self.X,self.Y,b,self.device,self.ls,self.min_points,self.nr_of_interpolation)
+        else:
+            try:
+                assert self.X.shape[0]==b.shape[0]
+            except AssertionError:
+                print('hey check the shapes of your tensor X and b they dont match up!')
+
+            if not self.smooth_interpolation:
+                if self.d==1:
+                    return load_obj.FFM_X_FLOAT_1(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.var_compression,self.eff_var_limit)
+                if self.d==2:
+                    return load_obj.FFM_X_FLOAT_2(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.var_compression,self.eff_var_limit)
+                if self.d==3:
+                    return load_obj.FFM_X_FLOAT_3(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.var_compression,self.eff_var_limit)
+                if self.d==4:
+                    return load_obj.FFM_X_FLOAT_4(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.var_compression,self.eff_var_limit)
+                if self.d==5:
+                    return load_obj.FFM_X_FLOAT_5(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.var_compression,self.eff_var_limit)
+            else:
+                if self.d==1:
+                    return load_obj.SUPERSMOOTH_FFM_X_FLOAT_1(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.eff_var_limit)
+                if self.d==2:
+                    return load_obj.SUPERSMOOTH_FFM_X_FLOAT_2(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.eff_var_limit)
+                if self.d==3:
+                    return load_obj.SUPERSMOOTH_FFM_X_FLOAT_3(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.eff_var_limit)
+                if self.d==4:
+                    return load_obj.SUPERSMOOTH_FFM_X_FLOAT_4(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.eff_var_limit)
+                if self.d==5:
+                    return load_obj.SUPERSMOOTH_FFM_X_FLOAT_5(self.X,b,self.device,self.ls,self.min_points,self.nr_of_interpolation,self.eff_var_limit)
+
+class benchmark_matmul():
+    def __init__(self,
+                 X,
+                 Y=None,
+                 ls=1.0,
+                 device = "cuda:0"):
+        self.X = X.to(device).float()
+        if torch.is_tensor(Y):
+            self.Y = Y.to(device).float()
+        else:
+            self.Y = Y
+        self.d = self.X.shape[1]
+        try:
+            assert self.d>0 and self.d<6
+        except AssertionError:
+            print('Sorry bro, dimensionality of your data is too big; Can only do up to 5')
+        self.ls = float(ls)
+        self.device = device
+
+    def update_ls(self,ls):
+        try:
+            assert ls>0
+        except AssertionError:
+            print('ls less than 0, not allowed')
+        self.ls = float(ls)
+
+
+    def __matmul__(self, b):
+        b = b.to(self.device).float()
+        if torch.is_tensor(self.Y):
+            try:
+                assert self.Y.shape[1]==self.X.shape[1]
+                assert self.Y.shape[0]==b.shape[0]
+            except AssertionError:
+                print('hey check the shapes of your tensor X,Y and b they dont match up!')
+
+            Y=self.Y
+        else:
+            Y=self.X
+            try:
+                assert self.X.shape[0]==b.shape[0]
+            except AssertionError:
+                print('hey check the shapes of your tensor X and b they dont match up!')
+        if self.d==1:
+            return load_obj.FFM.rbf_float_1(self.X,Y,b,self.ls,True)
+        if self.d==2:
+            return load_obj.FFM.rbf_float_1(self.X,Y,b,self.ls,True)
+        if self.d==3:
+            return load_obj.FFM.rbf_float_1(self.X,Y,b,self.ls,True)
+        if self.d==4:
+            return load_obj.FFM.rbf_float_1(self.X,Y,b,self.ls,True)
+        if self.d==5:
+            return load_obj.FFM.rbf_float_1(self.X,Y,b,self.ls,True)
+
+
+
+
+
+
--- try_extension.py
+++ try_extension.py
@@ -1,22 +1,46 @@
-import torch
-from torch.utils.cpp_extension import load
-FFM = load(name='ffm_3d_float', sources=['pybinder_setup.cu'])
+from FFM_classes import *
+import time
 if __name__ == '__main__':
     n=1000000
     device = "cuda:0"
     X = torch.randn(n,3).float().to(device)
     min_points = float(5000)
     x_ref = X[0:int(min_points)]
-    # Y = torch.randn(n,3).float().to(device)
+    Y = torch.rand(n,3).float().to(device)
     b = torch.randn(n,1).float().to(device)
-    ls = float(5.0)
-    nr_of_interpolation = int(30)
+    ls = float(10.0)
+    nr_of_interpolation = int(64)
     var_compression = True
     eff_var_limit=float(0.1)
-    y_ref = FFM.rbf_float_3(x_ref,X,b,ls,True)
-    # y1 =  FFM.FFM_X_FLOAT_3(X,b,device,ls,min_points,nr_of_interpolation,var_compression,eff_var_limit)
-    y1 =  FFM.SUPERSMOOTH_FFM_X_FLOAT_3(X,b,device,ls,min_points,nr_of_interpolation,eff_var_limit)
-    print(y_ref[0:10])
-    print(y1[0:10])
-    # print(y2[0:10])
+
+    FFM_X_0 = FFM(X=X,ls=ls,min_points=min_points,nr_of_interpolation=nr_of_interpolation,eff_var_limit=eff_var_limit,var_compression=False,smooth_interpolation=False)
+    FFM_X_1 = FFM(X=X,ls=ls,min_points=min_points,nr_of_interpolation=nr_of_interpolation,eff_var_limit=eff_var_limit,var_compression=True,smooth_interpolation=False)
+    FFM_X_2 = FFM(X=X,ls=ls,min_points=min_points,nr_of_interpolation=nr_of_interpolation,eff_var_limit=eff_var_limit,var_compression=True,smooth_interpolation=True)
+    FFM_XY_1 = FFM(X=X,Y=Y,ls=ls,min_points=min_points,nr_of_interpolation=nr_of_interpolation,eff_var_limit=eff_var_limit)
+
+    start = time.time()
+    res_0 = FFM_X_0@b
+    end= time.time()
+    print(f'time exp 0: {end-start}')
+
+    start = time.time()
+    res_1 = FFM_X_1@b
+    end= time.time()
+
+    print(f'time exp 1: {end-start}')
+
+
+    start = time.time()
+    res_2 = FFM_X_2@b
+    end= time.time()
+    print(f'time exp 2: {end-start}')
+
+
+    start = time.time()
+    res_3 = FFM_XY_1@b
+    end= time.time()
+
+    print(f'time exp 3: {end-start}')
+
+
 
