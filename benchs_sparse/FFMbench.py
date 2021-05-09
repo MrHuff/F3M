90f8b23c02b817c1591ae9d1f7b8cd27b0365026
Joan Glaunès <joan.glaunes@gmail.com>
2021-05-09 19:09:43 +0000

    updated, WIP

--- benchs_sparse/FFMbench.py
+++ benchs_sparse/FFMbench.py
@@ -7,10 +7,15 @@ from FFM_classes import *
 from run_obj import *
 import itertools
 
+import matplotlib.pyplot as plt
+from hover_scatter import hover_scatter
+from sparse_datasets import PlotData
 
+def dict2str(d):
+    return "\n".join(list(f"{k} : {v}" for k, v in d.items()))
 
-def FFMbench(X, Y, b, ls):
-
+def FFMbench(X, Y, b, ls, plot=False):
+    
     # compute reference with KeOps for error evaluation
     ref_points = 5000                         # calculate error on 5000 points
     x_ref = X[0:ref_points,:]                 # reference X
@@ -21,18 +26,31 @@ def FFMbench(X, Y, b, ls):
         res_ref = benchmark_matmul(x_ref,Y,ls=ls,device=device) @ b
 
     def call(**kwargs_list):
-        kwargs = kwargs_list
-        print(kwargs_list)
-        for params in itertools.product(*kwargs_list.values()):
-            for key, p in zip(kwargs,params):
+        elapsed = np.array([])
+        rel_err = np.array([])
+        kwargs_rec = []
+        for k,params in enumerate(itertools.product(*kwargs_list.values())):
+            kwargs = dict()
+            for key, p in zip(kwargs_list,params):
                 kwargs[key] = p
-            print("\ntesting with parameters :", kwargs)
+            kwargs_rec.append(kwargs)
+            print("\ntesting with parameters :", dict2str(kwargs))
             myFFM = FFM(X=X, Y=Y, ls=ls, device=device, **kwargs)
             start = time.time()
             res = myFFM @ b
             end = time.time()
+            elapsed = np.append(elapsed, end-start)
             print(f'elapsed: {end-start}')
-            rel_err = calc_rel_error(true_res=res_ref, approx_res=res[:ref_points])
-            print(f'error: {rel_err}')
+            rel_err = np.append(rel_err, calc_rel_error(true_res=res_ref, approx_res=res[:ref_points]).item())
+            print(f'error: {rel_err[-1]}')
+        if plot:
+            #plt.plot(elapsed[None,:], rel_err[None,:],'.')
+            #plt.legend(kwargs_rec)
+            names = list(dict2str(kwargs) for kwargs in kwargs_rec)
+            hover_scatter(elapsed, rel_err,names, plotfun=plt.loglog)
+            plt.xlabel('time(s)')
+            plt.ylabel('relative error')
+            plt.axis('equal')
+            plt.show()
     
     return call
--- benchs_sparse/bench_sparse_datasets.py
+++ benchs_sparse/bench_sparse_datasets.py
@@ -1,10 +1,15 @@
 import torch
 from sparse_datasets import PlotData, MaternClusterData, FBMData
 from FFMbench import FFMbench
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(1)
 
 #X = MaternClusterData(2,100,100,.05)
 
 X = FBMData(2,1000000,0.75)
+PlotData(X, max_npoints=10000)
+plt.axis('equal')
 
 device = "cuda:0"                         # device
 
@@ -13,7 +18,7 @@ n, dim = X.shape
 b = torch.randn(n,1).float().to(device)   # weights
 ls = float(1.0)                           # lengthscale
 
-bench_X = FFMbench(X,None,b,ls)
+bench_X = FFMbench(X,None,b,ls,plot=True)
 
 
 
@@ -25,13 +30,13 @@ bench_X = FFMbench(X,None,b,ls)
 # or increase min_points
 
 # nr of interpolation nodes
-nr_of_interpolation = [int(32), int(64), int(128)]
+nr_of_interpolation = [int(32), int(64), int(128), int(256)]
        
 # Effective variance threshold
-eff_var_limit = [float(0), float(0.1), float(0.15), float(0.2)]            
+eff_var_limit = [float(0.25), float(0.3), float(0.35), float(0.4), float(0.45)]            
 
 # stop when dividing when the largest box has 1000 points
-min_points = [float(500), float(1000), float(2000)]              
+min_points = [float(500), float(1000), float(2000), float(4000)]              
 
 var_compression = [True]
 
